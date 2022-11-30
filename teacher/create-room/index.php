a82e18e0218503f8596d11dd69b65172c69423be
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-11-30 21:45:37 +0100

    Fix to absolute link

--- teacher/create-room/index.php
+++ teacher/create-room/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database credentials
-require('/config.php');
+require('../../config.php');
 
 // Connect with the Credentials
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
