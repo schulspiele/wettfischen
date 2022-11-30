80181843633ddc68dfca301fc5bb7f39a21b8efc
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-11-30 21:41:07 +0100

    Fixed indentation

--- teacher/create-room/index.php
+++ teacher/create-room/index.php
@@ -20,42 +20,39 @@ if (mysqli_connect_errno()) {
 // Start the PHP_session
 session_start();
 
-    // Functions to generate an ID
-    function generateID($length){
-        $id_charset = "123465789";
-        $generated = "";
-        for ($i=0; $i < $length; $i++) { 
-            $new_char = substr($id_charset, mt_rand(0, strlen($id_charset)),1);
-            $generated .= $new_char;
-        }
-        return $generated;
+// Functions to generate an ID
+function generateID($length){
+    $id_charset = "123465789";
+    $generated = "";
+    for ($i=0; $i < $length; $i++) { 
+        $new_char = substr($id_charset, mt_rand(0, strlen($id_charset)),1);
+        $generated .= $new_char;
     }
+    return $generated;
+}
 
-    // Check if id is already taken
-    $checking_id_double = true;
-    while($checking_id_double){  
-        $new_id = generateID(4);
-        if ($stmt = $con->prepare("SELECT * FROM sessions WHERE id = ?")) {
-            $stmt->bind_param('s', $new_id);
+// Check if id is already taken
+$checking_id_double = true;
+while($checking_id_double){  
+    $new_id = generateID(4);
+    if ($stmt = $con->prepare("SELECT * FROM sessions WHERE id = ?")) {
+        $stmt->bind_param('s', $new_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+        } else {
+            $checking_id_double = false;
+            $new_pass = generateID(4);
+            $stmt = $con->prepare("INSERT INTO sessions (namecode, passcode) VALUES (?, ?)");
+            $stmt->bind_param('ss', $new_id, $new_pass);
             $stmt->execute();
-            $result = $stmt->get_result();
-            if ($result->num_rows > 0) {
-            } else {
-                $checking_id_double = false;
-                $new_pass = generateID(4);
-                $stmt = $con->prepare("INSERT INTO sessions (namecode, passcode) VALUES (?, ?)");
-                $stmt->bind_param('ss', $new_id, $new_pass);
-                $stmt->execute();
-                $stmt->close();
-                $con->close();
-                $_SESSION['id'] = $new_id;
-                $_SESSION['pass'] = $new_pass;
-                header("Location: ../room/");
-            }
+            $stmt->close();
+            $con->close();
+            $_SESSION['id'] = $new_id;
+            $_SESSION['pass'] = $new_pass;
+            header("Location: ../room/");
         }
     }
-
-// close the database connection
-$con->close();
+}
 
 ?>
