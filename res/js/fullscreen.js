8d3d2675d43adb1fa753f7cb7bd423afa4598010
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-01 17:27:56 +0100

    Add overlay buttons

---
+++ res/js/fullscreen.js
@@ -0,0 +1,7 @@
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+}
