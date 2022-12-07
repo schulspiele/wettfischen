dcff79230439d9700a6dd5e6590bb2498050ef6d
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-07 16:34:11 +0100

    Add student room join page

---
+++ student/room/index.php
@@ -0,0 +1,47 @@
+<?php 
+
+    session_start();
+    require('../../config.php');
+
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    if (mysqli_connect_errno()) exit("Error");
+
+    if(!isset($_GET['r']) || !isset($_GET['c'])) header("Location: ../");
+    $id = $_GET['r'];
+    $pass = $_GET['c'];
+
+    if ($stmt = $con->prepare("SELECT * FROM rooms WHERE namecode = ? AND passcode = ?")) {
+        $stmt->bind_param('ss', $id, $pass);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows == 0) header("Location: ../join-room/");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wettfischen</title>
+    <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
+    <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/res/fontawesome/css/solid.min.css">
+    <link rel="stylesheet" href="/res/css/fonts.css">
+    <link rel="stylesheet" href="/res/css/main.css">
+    <link rel="stylesheet" href="/res/css/student/room.css">
+</head>
+
+<body>
+    <main>
+        <h1>
+            Bitte warte, bis dein(e) Lehrer*in die Runde startet.
+        </h1>
+    </main>
+    <script src="/res/js/student/validate_displayname.js"></script>
+    <script src="/res/js/jquery/jquery-3.6.1.min.js"></script>
+</body>
+
+</html>
