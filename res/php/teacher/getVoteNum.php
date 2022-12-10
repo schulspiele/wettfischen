964845bb3b57f70cdcce0f6516f5fa865be87212
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-10 12:18:29 +0100

    Fix column name

--- res/php/teacher/getVoteNum.php
+++ res/php/teacher/getVoteNum.php
@@ -10,7 +10,7 @@
     $room = $_SESSION['id'];
 
     // Get number of users in room that are in the same round as the "round" column in the rooms table and voted is not 0
-    if ($stmt = $con->prepare("SELECT COUNT(*) FROM users WHERE room = ? AND round = (SELECT round FROM rooms WHERE namecode = ?) AND voted != 0")) {
+    if ($stmt = $con->prepare("SELECT COUNT(*) FROM users WHERE room = ? AND round = (SELECT round FROM rooms WHERE namecode = ?) AND votes != 0")) {
         $stmt->bind_param('ss', $room, $room);
         $stmt->execute();
         $stmt->bind_result($num_users);
