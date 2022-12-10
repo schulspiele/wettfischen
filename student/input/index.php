e848bb51302c1167fc1f73e703f085746ca0c53a
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-10 12:49:33 +0100

    Remove ability to zoom

--- student/input/index.php
+++ student/input/index.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <title>Wettfischen</title>
     <link rel="icon" type="image/x-icon" href="/res/img/favicon.ico" />
     <link rel="stylesheet" href="/res/fontawesome/css/fontawesome.min.css">
