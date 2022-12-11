efcf049ddb6cbcd925f391f4379c34250a2813b1
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-11 18:20:31 +0100

    Add room status (ready to delete) setting

--- teacher/end_room/index.php
+++ teacher/end_room/index.php
@@ -1,6 +1,25 @@
 <?php
 
     session_start();
+
+    if(!isset($_SESSION['id']) || !isset($_SESSION['pass'])){
+        header("Location: ../");
+    } else {
+        $id = $_SESSION['id'];
+        $pass = $_SESSION['pass'];
+    }
+
+    require('../../config.php');
+
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    if (mysqli_connect_errno()) exit("DB-Error");
+
+    if ($stmt = $con->prepare("UPDATE rooms SET status = 3 WHERE namecode = ?")) {
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $stmt->close();
+    }
+
     session_destroy();
 
     header("Location: ../../");
