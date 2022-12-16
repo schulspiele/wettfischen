0a427f693184c429cf96bfecc4cd78242e624bff
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-16 16:17:05 +0100

    Fix between_rounds not setting status

--- teacher/between_rounds/index.php
+++ teacher/between_rounds/index.php
@@ -1,3 +1,26 @@
+<?php 
+
+    session_start();
+
+    if(!isset($_SESSION['id']) || !isset($_SESSION['pass'])){
+        header("Location: ../");
+    } else {
+        $id = $_SESSION['id'];
+        $pass = $_SESSION['pass'];
+    }
+
+    require('../../config.php');
+
+    $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    if (mysqli_connect_errno()) exit("DB-Error");
+    
+    if($stmt = $con->prepare("UPDATE rooms SET status = 5 WHERE namecode = ?")){
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $stmt->close();
+    } else exit("DB-Error");
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 
