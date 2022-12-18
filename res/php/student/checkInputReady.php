09f67585cde3d1a4c2d57716fce2b0c73ab29386
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-12-18 20:43:57 +0100

    Fix status checking

--- res/php/student/checkInputReady.php
+++ res/php/student/checkInputReady.php
@@ -6,18 +6,29 @@
     $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     if (mysqli_connect_errno()) exit("Error");
 
-    if(!isset($_SESSION['displayname']) || !isset($_POST['room'])) exit("displayname or room");
-    $displayname = $_SESSION['displayname'];
+    if(!isset($_SESSION['id']) || !isset($_POST['room'])) exit("id or room");
+    $id = $_SESSION['id'];
     $room = $_POST['room'];
 
-    if($stmt = $con->prepare("SELECT status FROM rooms WHERE namecode = ?")) {
+    // Get status AND check if round is not the same
+    if($stmt = $con->prepare("SELECT status, round FROM rooms WHERE namecode = ?")){
         $stmt->bind_param("i", $room);
         $stmt->execute();
-        $stmt->bind_result($status);
+        $stmt->bind_result($status, $room_round);
         $stmt->fetch();
-        echo $status;
-        $_SESSION["room"] = $room;
         $stmt->close();
-        exit();
+        if($stmt = $con->prepare("SELECT round FROM users WHERE id = ?")){
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $stmt->bind_result($user_round);
+            $stmt->fetch();
+            $stmt->close();
+            if($user_round == $room_round){
+                echo "NO";
+            } else {
+                echo $status;
+            }
+        
+        } else exit("DB-Error");
     }
 ?>
